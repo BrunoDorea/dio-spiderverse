687d40ef05f0176efa314c8265966decac62320e
Bruno Henrique <brunodorea@outlook.com.br>
2023-07-05 21:16:10 -0300

    📦 build: desenvolvimento do carrossel

--- src/components/HeroesList/index.tsx
+++ src/components/HeroesList/index.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 import HeroPicture from "../HeroPicture";
 
@@ -14,21 +17,35 @@ interface IProps {
 export default function HeroesList({ heroes }: IProps) {
   return (
     <>
-      <h1 className={`${spidermanFont.className} ${styles.title}`}>
+      <motion.h1
+        className={`${spidermanFont.className} ${styles.title}`}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2, delay: 2 }}
+      >
         Personagens
-      </h1>
-      <section className={styles.heroes}>
+      </motion.h1>
+
+      <motion.section
+        className={styles.heroes}
+        initial={{ opacity: 0, y: -100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 2 }}
+      >
         {heroes.map((hero) => (
-          <div
+          <motion.div
             key={hero.id}
             className={`${styles.imageContainer} ${styles[hero.id]}`}
+            transition={{ duration: 0.8 }}
+            whileHover={{ scale: 1.3 }}
+            whileTap={{ scale: 0.8 }}
           >
             <Link href={`/hero/${hero.id}`}>
               <HeroPicture hero={hero} />
             </Link>
-          </div>
+          </motion.div>
         ))}
-      </section>
+      </motion.section>
     </>
   );
 }
